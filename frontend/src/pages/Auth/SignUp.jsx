7bea92def6eeaf043b303cd7391faf73a5bafce0
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-05-11 00:07:18 -0400

    Implemented login page design

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/inputs/Input";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = () => {
+  // 34:27 timestamp
+
   return <div>SignUp</div>;
 };
 
